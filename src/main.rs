55ba4ffe1c75fcc888f0e94eb789bad6121a03ca
Eric Hodel <drbrain@segment7.net>
2022-01-03 13:35:45 -0800

    Add process_start_time_seconds

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ mod sensor;
 use anyhow::anyhow;
 use anyhow::Result;
 
+use lazy_static::lazy_static;
+
 use log::error;
 
 use configuration::Configuration;
@@ -18,10 +20,26 @@ use downloader::Downloader;
 use exporter::Exporter;
 use flume_builder::FlumeBuilder;
 
+use prometheus::register_gauge;
+use prometheus::Gauge;
+
 use tokio::sync::mpsc;
+
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
+
+lazy_static! {
+    static ref START_TIME: Gauge = register_gauge!(
+        "process_start_time_seconds",
+        "Start time of the process since unix epoch in seconds."
+    )
+    .unwrap();
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let start_time = SystemTime::now().duration_since(UNIX_EPOCH).ok();
+
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let configuration = Configuration::load_from_next_arg()?;
@@ -45,6 +63,10 @@ async fn main() -> Result<()> {
     Exporter::new(configuration.bind_address())?
         .start(error_tx.clone())
         .await;
+
+    if let Some(duration) = start_time {
+        START_TIME.set(duration.as_secs_f64());
+    }
 
     let exit_code = wait_for_error(error_rx).await;
 
